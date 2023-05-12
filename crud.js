da07ef4eeb7e6bdd9c9b37c8fb41be24f9a3a02e
evelyn1008 <evelyn.dryellen1008@gmail.com>
2023-05-12 09:42:31 -0300

    crud

--- crud.js
+++ crud.js
@@ -1,24 +1,56 @@
 document.querySelector("#salvar").addEventListener("click", cadastrar)
 
-function cadastrar(){
+let tarefas = []
+
+window.addEventListener("load",() => {
+  tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+  atualizar()
+  
+
+})
+document.querySelector("#busca").addEventListener("keyup", ()=> {
+  let busca = document.querySelector("#busca").value
+  let tarefasFiltradas = tarefas.filter((tarefa) => {
+    return tarefa.titulo.toLowerCase().includes(busca.toLowerCase())
+  })
+  filtrar(tarefasFiltradas)
+})
+
+function filtrar(tarefas){
+  document.querySelector("#tarefas").innerHTML = ""
+  tarefas.forEach((tarefa) =>{document.querySelector("#tarefas").innerHTML += createCard(tarefa)
+})
+}
+function atualizar(){
+  document.querySelector("#tarefas").innerHTML = ""
+  localStorage.setItem("tarefas", JSON.stringify (tarefas))
+  tarefas.forEach((tarefa) =>{document.querySelector("#tarefas").innerHTML += createCard(tarefa)
+})
+}
+  function cadastrar(){
     const titulo = document.querySelector("#titulo").value
     const descricao = document.querySelector("#descricao").value
     const categoria = document.querySelector("#categoria").value
     const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
 
     const tarefa = {
+        id: Date.now(),
         titulo: titulo,
         descricao: descricao,
-        categoria: categoria
+        categoria: categoria,
+        concluida: false
     }
     if(!validar(tarefa.titulo, document.querySelector("#titulo")))return
     if(!validar(tarefa.descricao, document.querySelector("#descricao")))return
 
-    document.querySelector("#tarefas").innerHTML += createCard(tarefa)
+    tarefas.push(tarefa)
+    
+    atualizar()
 
     modal.hide()
     
 }
+
 
 function validar(valor, campo){
     //fail fast
@@ -33,10 +65,27 @@ function validar(valor, campo){
     
 
 }
-function apagar(botao){
-    botao.parentNode.parentNode.parentNode.remove()
+function apagar(id){
+    tarefas = tarefas.filter((tarefa) =>{
+      return tarefa.id != id
+    })
+
+    atualizar()
+}
+function concluir(id){
+  let tarefaEncontrada = tarefas.find((tarefa) => {
+    return tarefa.id == id
+  })
+  tarefaEncontrada.concluida = true
+  atualizar()
 }
 function createCard(tarefa){
+
+    let disabled = tarefa.concluida ? "disabled" : ""
+
+
+
+  
     return `
     <div class="col-lg-3 col-md-6 col-12">
     <div class="card">
@@ -48,10 +97,10 @@ function createCard(tarefa){
         <p>
           <span class="badge text-bg-danger">${tarefa.categoria}</span>
         </p>
-        <a href="#" class="btn btn-success" title="marcar como concluida">
+        <a onClick="concluir(${tarefa.id})" href="#" class="btn btn-success ${disabled}" title="marcar como concluida">
           <i class="bi bi-check-lg"></i>
         </a>
-        <a onClick="apagar(this)" href="#" class="btn btn-danger" title="apagar tarefa">
+        <a onClick="apagar(${tarefa.id})" href="#" class="btn btn-danger" title="apagar tarefa">
           <i class="bi bi-trash"></i>
         </a>
       </div>
